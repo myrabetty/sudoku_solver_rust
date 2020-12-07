4d057fa8f63180c48533f06d2c8e80a8063c060e
esemboloni <esemboloni@schubergphilis.com>
2020-12-07 18:37:19 +0100

    creating template committing a got checkpoint

--- src/template/template.rs
+++ src/template/template.rs
@@ -2,22 +2,29 @@
 mod tests {
     use handlebars::Handlebars;
     use serde_json::json;
+
+    //```construct the current state of the sudoku given the current values and the unset possible values
+    fn show_grid(){
+
+    }
+
     #[test]
-    fn display_template() {
+    fn display_template_test() {
 
         let data = &json!({
-            "house_row": [
-                {"house": [
-                    {"cell":
-                        {"value":1}
-                    },
-                    {"cell":
-                        {"value":1}
-                    },
-                    {"cell":
-                        {"value":1}
-                    },
-                ]},
+                "house": [
+                    {"cell":[
+                    {"value":1},
+                    {"value":2},
+                    {"value":3},
+                    {"value":4},
+                    {"value":5},
+                    {"value":6},
+                    {"value":7},
+                    {"value":8},
+                    {"pencil-marks":[1,2,4]}
+                    ]},
+                    //{"cell":[1,2,3,4,5,6,7,8,9]},
             ]
         });
 
